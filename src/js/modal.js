fd4973bc5e897c99989fdaa963d6162d9c2202c6
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-17 13:52:43 +0200

    fix: delete modal

--- src/js/modal.js
+++
@@ -1,9 +0,0 @@
-const newsLetterContainer = document.querySelector('.newsletter-container')
-
-document.addEventListener("DOMContentLoaded", function(){
-  MicroModal.init({
-    onClose: () => {
-      newsLetterContainer.classList.add
-    }
-  });
-})
